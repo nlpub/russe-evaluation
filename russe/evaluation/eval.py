ae48d8cf6bf2e9fdf6cab9f9cb01138d485746e8
Alexander Panchenko <panchenko.alexander@gmail.com>
2015-01-06 23:10:50 +0300

    eval hj done

---
+++ russe/evaluation/eval.py
@@ -0,0 +1,48 @@
+
+import argparse
+from pandas import read_csv
+from scipy.stats import pearsonr, spearmanr
+
+
+HJ_FILE = "hj-submission.csv"
+SRC_FILE = "src-submission.csv"
+
+
+def hj_evaluation(args):
+    print "======================================================="
+    print "Evaluation based on correlations with human judgements"
+    print "See Section 1.1 of http://russe.nlpub.ru/task\n"
+
+    print "Input file:", args.hj_fpath
+    hj_df = read_csv(args.hj_fpath, ',', encoding='utf8')
+    print "Spearman's correlation with human judgements:\t%.5f (p-value = %.3f)" % spearmanr(hj_df.hj, hj_df.sim)
+    print "Pearson's correlation with human judgements:\t%.5f (p-value = %.3f)" % pearsonr(hj_df.hj, hj_df.sim)
+
+    return spearmanr(hj_df.hj, hj_df.sim)[0]
+
+
+def semantic_relation_classification_evaluation(args):
+    print "======================================================="
+    print "Evaluation based on semantic relation classificaton"
+    print "See Section 1.2 of http://russe.nlpub.ru/task\n"
+
+    raise NotImplementedError()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='RUSSE Evaluation Script. See http://russe.nlpub.ru for more details.')	
+    subparsers = parser.add_subparsers(description='Help for subcommand.')
+    parser_hj = subparsers.add_parser('hj', description='Evaluation based on correlations with human judgements.')
+    parser_hj.set_defaults(func=hj_evaluation)
+    parser_hj.add_argument('--hj_fpath', help='A CSV file in the format "word1,word2,hj,sim" e.g. ' + HJ_FILE, default=HJ_FILE)
+
+    parser_hj = subparsers.add_parser('src', description='Evaluation based on semantic relation classification.')
+    parser_hj.set_defaults(func=semantic_relation_classification_evaluation)
+    parser_hj.add_argument('--src_fpath', help='A CSV file in the format "word1,word2,related,sim" e.g. ' + SRC_FILE, default=SRC_FILE)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
