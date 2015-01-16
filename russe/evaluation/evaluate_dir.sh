c53144bb6447ec32dc0969c0f63f16d88640d6f3
Alexander Panchenko <panchenko.alexander@gmail.com>
2015-01-16 20:26:08 +0300

    dir fix

--- russe/evaluation/evaluate_dir.sh
+++ russe/evaluation/evaluate_dir.sh
@@ -4,14 +4,10 @@ for submission in `find $1 -maxdepth 1 -type d | sort` ; do
 	testcsv="$submission/test.csv"
 	if [ -a $testcsv ] ; then
 		echo $testcsv
-		./evaluation_test.py $testcsv > $submission/report.txt 2> $submission/results.txt
+		./evaluate_test.py $testcsv > $submission/report.txt 2> $submission/results.txt
 	fi    
 done
 
-
-# print entire results table :wqЖ:
-# read all results.txt
-# save the results
 echo "===================================================="
 find $1 -maxdepth 2 -iname "results.txt" -exec echo "" \; -exec ls '{}' \; -exec cat '{}' \;
 
